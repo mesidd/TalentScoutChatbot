ae3215982861bf00aff7038f4f382c5e85606b1f
kinjal-mitra <kinjalmitra03@gmail.com>
2025-08-26 20:33:41 +0530

    Upload config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+QUESTIONS = [
+    "Please enter your Full Name",
+    "Please enter your e-mail address",
+    "Please enter your phone number",
+    "Please enter Years of Experience you have in your desired field",
+    "Please enter your desired position(s)",
+    "Please enter the Tech Stack(s) you are familiar with. Note that your subsequent interview will based on the basis of your tech stack"
+]  
